632d36035ef713bbb87e2071c277d28f540b886f
Jason Lee <jasonleejin@gmail.com>
2020-06-08 17:50:04 -0400

    updated weather message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location');
         } else {
-            callback(undefined, `It is currently ${body.current.temperature} out. feels like ${body.current.feelslike}`);
+            callback(undefined, 
+                body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + 
+                " degrees out. It feels like " + body.current.feelslike + " degrees out. The humidity is " +
+                body.current.humidity + "%.");
         }
     });
 }
